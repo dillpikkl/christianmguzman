c6427ade1ff42546eeb296cc6ff549d3df6e5ccd
dillpikkl <bossbamgames@gmail.com>
2022-05-24 21:22:33 -0700

    added comments

--- home.js
+++ home.js
@@ -1,10 +1,11 @@
-
+//cool secret. will add more soon.
 window.addEventListener("keydown", function(event) {
     if (event.key == "h"){
         alert("*cue zelda discovery sound*");
     }
 });
 
+//checks for key
 if (document.cookie == "A4pBl") {
     console.log("allowed")
     
